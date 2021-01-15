dae746183ebc2b7a1aaee36ba3755e18c8dde9e5
ilromali <iliashenkoroman@gmail.com>
2021-01-15 13:36:11 -0800

    Delete & rebuild program files b/c of bug

---
+++ Bakery/Startup.cs
@@ -0,0 +1,58 @@
+using Bakery.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Bakery
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+        var builder = new ConfigurationBuilder()
+            .SetBasePath(env.ContentRootPath)
+            .AddJsonFile("appsettings.json");
+        Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; set; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+        services.AddMvc();
+
+        services.AddEntityFrameworkMySql()
+            .AddDbContext<BakeryContext>(options => options
+            .UseMySql(Configuration["ConnectionStrings:DefaultConnection"]));
+
+        services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<BakeryContext>()
+                .AddDefaultTokenProviders();
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+        app.UseStaticFiles();
+
+        app.UseDeveloperExceptionPage();
+
+        app.UseAuthentication();
+
+        app.UseMvc(routes =>
+        {
+            routes.MapRoute(
+            name: "default",
+            template: "{controller=Home}/{action=Index}/{id?}");
+        });
+
+        app.Run(async (context) =>
+        {
+            await context.Response.WriteAsync("Uh-oh, something went wrong!");
+        });
+        }
+    }
+}
