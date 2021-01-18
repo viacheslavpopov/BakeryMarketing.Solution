113687aafa6510350830a192fe33cdc43355e996
ilromali <iliashenkoroman@gmail.com>
2021-01-17 20:51:30 -0800

    Add carousel javascript, style, & jQuery links

--- Bakery/wwwroot/js/main.js
+++ Bakery/wwwroot/js/main.js
@@ -1,3 +1,23 @@
-$('.carousel').carousel({
-    interval: 5000
-})
+$(document).ready(function(){
+    // Activate Carousel
+    $('.carousel').carousel({
+        interval: 5000
+    })
+    // Enable Carousel Indicators
+    $(".item1").click(function(){
+        $("#homeCarousel").carousel(0);
+    });
+    $(".item2").click(function(){
+        $("#homeCarousel").carousel(1);
+    });
+    $(".item3").click(function(){
+        $("#homeCarousel").carousel(2);
+    });
+    // Enable Carousel Controls
+    $(".carousel-control-prev").click(function(){
+        $("#homeCarousel").carousel("prev");
+    });
+    $(".carousel-control-next").click(function(){
+        $("#homeCarousel").carousel("next");
+    });
+});
