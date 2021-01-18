e2f7f463432bc01164c5b146be2b18db2e96c2a3
ilromali <iliashenkoroman@gmail.com>
2021-01-17 20:50:16 -0800

    Add lists to splash page

--- Bakery/Controllers/HomeController.cs
+++ Bakery/Controllers/HomeController.cs
@@ -1,12 +1,25 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Bakery.Models;
+using Microsoft.AspNetCore.Mvc;
+// using Microsoft.AspNetCore.Mvc.Rendering;
+// using Microsoft.EntityFrameworkCore;
+// using System.Collections.Generic;
+using System.Linq;
 
 namespace Bakery.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly BakeryContext _db;
+        public HomeController(BakeryContext db)
+        {
+            _db = db;
+        }
+
         [HttpGet("/")]
         public ActionResult Index()
         {
+            ViewBag.Flavors = _db.Flavors.ToList();
+            ViewBag.Sweets = _db.Sweets.ToList();
             return View();
         }
     }
