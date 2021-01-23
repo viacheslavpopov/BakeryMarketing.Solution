07926cd1ce75066e091ddbbf517515573a7835e6
ilromali <iliashenkoroman@gmail.com>
2021-01-23 13:09:42 -0800

    Add required fields & data annotations for login/register

--- Bakery/ViewModels/LoginViewModel.cs
+++ Bakery/ViewModels/LoginViewModel.cs
@@ -1,8 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Bakery.ViewModels
 {
     public class LoginViewModel
     {
+        [Required]
         public string Email { get; set; }
+
+        [Required]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
         public string Password { get; set; }
     }
 }
